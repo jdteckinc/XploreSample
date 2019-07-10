6d4e7aa0874943932e9bc7950dabb9bd11eac47d
Dinesh <kumardinesh.viswanath@gmail.com>
2019-07-11 00:24:23 +0800

    swagger ui changes

--- microService/src/main/java/com/xplore/controller/XPloreController.java
+++ microService/src/main/java/com/xplore/controller/XPloreController.java
@@ -7,15 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.xplore.bean.XploreResponse;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/testService")
+@Api(value = "test Servcie", description = "Operations pertaining to products in Online Store")
 public class XPloreController {
 
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public XploreResponse getResonse(@RequestParam(value = "name") String name) {
+	@ApiOperation(value = "View a list of available employees", response = XploreResponse.class)
+	public XploreResponse getResonse(
+			@ApiParam(value = "Employee Id to update employee object", required = true) @RequestParam(value = "name") String name) {
 		XploreResponse response = new XploreResponse();
 		response.setName(name);
-		response.setMessage("Welcome "+ name + " !");
+		response.setMessage("Welcome " + name + " !");
 		return response;
 	}
 }
